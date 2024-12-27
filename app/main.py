93357d3b45b737a07003eb6416b34161475eee7e
Shiven <shivenkumar22@gmail.com>
2024-12-27 21:21:51 +0530

    Fix: Frontend URL bug

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://https://shiven-is-shiven.vercel.app/",  # Your actual Vercel URL
+        "https://shiven-is-shiven.vercel.app", 
     ],
     allow_credentials=True,
     allow_methods=["*"],
